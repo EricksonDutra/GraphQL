b2bc84f519d14e3b1ca7bb4a8a090bb17637d9cf
EricksonDutra <ericksond10@gmail.com>
2021-12-31 13:37:27 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -3,23 +3,11 @@ import { ApolloServer, gql } from 'apollo-server';
 const server = new ApolloServer({
   typeDefs: gql`
     type Query {
-      id: ID!
-      name: String
-      age: Int
-      average: Float
-      married: Boolean!
-      arrayString: [String]
+      user: User
     }
   `,
   resolvers: {
-    Query: {
-      id: () => '1sadia189-654asdf-f',
-      name: () => 'Erickson Dutra',
-      age: () => 31,
-      average: () => 31.22,
-      married: () => true,
-      arrayString: () => ['G', 'R', 'A', 'P', 'H', 'Q', 'L'],
-    },
+    Query: {},
   },
 });
 
