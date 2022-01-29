3043c07d91d2f98750aeb3571a6d02aba9208092
EricksonDutra <ericksond10@gmail.com>
2022-01-28 20:09:38 -0400

    Interface Type

--- src/graphql/post/typedefs.js
+++ src/graphql/post/typedefs.js
@@ -5,18 +5,26 @@ export const postTypeDefs = gql`
     post(id: ID!): PostResult!
     posts(input: ApiFiltersInput): [Post!]!
   }
-
-  union PostResult = PostNotFoundError | Post
-  type PostNotFoundError {
+  union PostResult = PostNotFoundError | PostTimeoutError | Post
+  interface PostError {
     statusCode: Int!
     message: String!
   }
-
+  type PostNotFoundError implements PostError {
+    statusCode: Int!
+    message: String!
+    postId: String!
+  }
+  type PostTimeoutError implements PostError {
+    statusCode: Int!
+    message: String!
+    timeout: Int!
+  }
   type Post {
     id: ID!
     title: String!
     body: String!
-    #user: User!
+    # user: User!
     indexRef: Int!
     createdAt: String!
     unixTimestamp: String!
